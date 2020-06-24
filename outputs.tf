35770221612fbfb5f80931ed2ffd0c78ef1a3b31
jslopes <jslopes@uoldiveo.com>
2020-06-24 15:16:25 -0300

    fix main.tf

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,10 @@ output "iam_user" {
     value   = aws_iam_user.new-user[0].name 
 }
 output "secret" {
-    value   = aws_iam_access_key.access-key[0].secret
+    value   = length(aws_iam_access_key.access-key) > 0 ? aws_iam_access_key.access-key[0].secret : null
 }
 output "access_key" {
-    value   = aws_iam_access_key.access-key[0].id
+    value   = length(aws_iam_access_key.access-key) > 0 ? aws_iam_access_key.access-key[0].id : null
 }
 output "encrypted_password" {
     value   = aws_iam_user_login_profile.login-profile[0].encrypted_password
